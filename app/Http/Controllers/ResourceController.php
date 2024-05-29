743ff619abd41d00a83d77d8f5623b87c80f1777
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-29 17:34:00 +0200

    Edited ResourceController.php, web.php, welcome.blade.php is now comics.blade.php

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ResourceController extends Controller
 {
-    //
+    public function comics() {
+
+        $comics = Comic::all();
+
+        return view('comics', compact('comics'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ResourceController::class, 'comics'])->name('comics');
