17c91b3f744af52cdca8def41afb92ebfd043ba8
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-29 19:53:51 +0200

    Added show.blade.php

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title','{{$comic->title}}')
+@section('content')
+    <div class="container text-center">
+        <h1>{{$comic->title}}</h1>    
+    </div>
+    <div class="container">
+        <div class="my-4">
+            <div class="card">
+                <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center">{{$comic->title}}</h5>
+                    <p class="card-text">{{$comic->series}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
