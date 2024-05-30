1ad07db6ea7458ba29438ee7268dff40662a20c5
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-30 15:54:21 +0200

    Added store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -25,7 +25,28 @@ class ComicController extends Controller
         return view('comics.create');
     } 
 
-    public function store(){
-        dump('metodo store');
+    public function store(Request $request){
+
+        $form_data = $request->all();
+
+        $new_comic = Comic::create($form_data);
+
+        //MI LASCIO ANCHE LA VERSIONE ESTESA PER RICORDARMELA
+        
+        // $new_comic = new Comic();
+
+        // $new_comic->title = $form_data['title'];
+        // $new_comic->description = $form_data['description'];
+        // $new_comic->thumb = $form_data['thumb'];
+        // $new_comic->price = $form_data['price'];
+        // $new_comic->series = $form_data['series'];
+        // $new_comic->sale_date = $form_data['sale_date'];
+        // $new_comic->type = $form_data['type'];
+        // $new_comic->artists = $form_data['artists'];
+        // $new_comic->writers = $form_data['writers'];
+
+        // $new_comic->save();
+
+        return redirect()->route('comics.show', $new_comic);
     }
 }
